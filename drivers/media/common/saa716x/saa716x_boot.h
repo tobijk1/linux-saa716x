622d44fb12adfb1bebd1c0a55a359545ddb01c73
Manu Abraham <manu@linuxtv.org>
2010-01-04 22:48:26 +0400

    saa716x: Partial rework of SPI I/O

--- drivers/media/common/saa716x/saa716x_boot.h
+++ drivers/media/common/saa716x/saa716x_boot.h
@@ -6,6 +6,8 @@ enum saa716x_boot_mode {
 	SAA716x_INT_BOOT, /* GPIO[31:30] = 0x01 */
 };
 
+struct saa716x_dev;
+
 extern int saa716x_core_boot(struct saa716x_dev *saa716x);
 extern int saa716x_jetpack_init(struct saa716x_dev *saa716x);
 extern void saa716x_core_reset(struct saa716x_dev *saa716x);
