24bd94f915f9e9cc4fed99a2075f521d3d0e9067
Manu Abraham <manu@linuxtv.org>
2010-01-04 16:42:56 +0400

    saa716x: Add GREG routines

---
+++ drivers/media/common/saa716x/saa716x_greg.h
@@ -0,0 +1,7 @@
+#ifndef __SAA716x_GREG_H
+#define __SAA716x_GREG_H
+
+extern void saa716x_greg_save(struct saa716x_dev *saa716x);
+extern void saa716x_greg_restore(struct saa716x_dev *saa716x);
+
+#endif /* __SAA716x_GREG_H */
