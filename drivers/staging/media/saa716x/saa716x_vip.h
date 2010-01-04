c7ab434b4874c5606eb5e07b0cbe554855054cdd
Manu Abraham <manu@linuxtv.org>
2010-01-04 15:50:42 +0400

    saa716x: Still scratching
    [moved from media/common/saa716x/ to staging/media/saa716x/]
    Signed-off-by: Soeren Moch <smoch@web.de>

---
+++ drivers/staging/media/saa716x/saa716x_vip.h
@@ -0,0 +1,7 @@
+#ifndef __SAA716x_VIP_H
+#define __SAA716x_VIP_H
+
+extern void saa716x_vipint_disable(struct saa716x_dev *saa716x);
+extern void saa716x_vip_disable(struct saa716x_dev *saa716x);
+
+#endif /* __SAA716x_VIP_H */
