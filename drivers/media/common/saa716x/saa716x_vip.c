0f57d23d5dad4c42af04d11e6c8b7e836429317a
Manu Abraham <manu@linuxtv.org>
2010-01-04 15:49:51 +0400

    saa716x: Update register definitions

--- drivers/media/common/saa716x/saa716x_vip.c
+++ drivers/media/common/saa716x/saa716x_vip.c
@@ -3,9 +3,9 @@
 
 void saa716x_vipint_disable(struct saa716x_dev *saa716x)
 {
-	SAA716x_WR(VI0, 0xfe4, 0); /* disable VI 0 IRQ */
-	SAA716x_WR(VI1, 0xfe4, 0); /* disable VI 1 IRQ */
-	SAA716x_WR(VI0, 0xfe8, 0x3ff); /* clear IRQ */
-	SAA716x_WR(VI1, 0xfe8, 0x3ff); /* clear IRQ */
+	SAA716x_WR(VI0, INT_ENABLE, 0); /* disable VI 0 IRQ */
+	SAA716x_WR(VI1, INT_ENABLE, 0); /* disable VI 1 IRQ */
+	SAA716x_WR(VI0, INT_CLR_STATUS, 0x3ff); /* clear IRQ */
+	SAA716x_WR(VI1, INT_CLR_STATUS, 0x3ff); /* clear IRQ */
 }
 EXPORT_SYMBOL_GPL(saa716x_vipint_disable);
