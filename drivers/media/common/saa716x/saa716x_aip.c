206fd9adcf19a1e6ab056ddb98039fb5dfdafcb7
Manu Abraham <manu@linuxtv.org>
2010-01-04 15:56:19 +0400

    saa716x: Enable more debugging

---
+++ drivers/media/common/saa716x/saa716x_aip.c
@@ -0,0 +1,16 @@
+#include "saa716x_reg.h"
+#include "saa716x_priv.h"
+#include "saa716x_aip.h"
+
+int saa716x_aip_status(struct saa716x_dev *saa716x, u32 dev)
+{
+	return SAA716x_RD(dev, AI_CTL) == 0 ? 0 : -1;
+}
+EXPORT_SYMBOL_GPL(saa716x_aip_status);
+
+void saa716x_aip_disable(struct saa716x_dev *saa716x)
+{
+	SAA716x_WR(AI0, AI_CTL, 0x00);
+	SAA716x_WR(AI1, AI_CTL, 0x00);
+}
+EXPORT_SYMBOL_GPL(saa716x_aip_disable);
