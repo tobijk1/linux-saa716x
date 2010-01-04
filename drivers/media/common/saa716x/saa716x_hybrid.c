7c335fe35ef6699c5606ae921b280e4a756f9694
Manu Abraham <manu@linuxtv.org>
2010-01-04 12:42:05 +0400

    saa716x: Updated board definitions
    From Jacky

--- drivers/media/common/saa716x/saa716x_hybrid.c
+++ drivers/media/common/saa716x/saa716x_hybrid.c
@@ -67,19 +67,44 @@ static int load_config_vp6090(struct saa716x_dev *saa716x)
 	return ret;
 }
 
+static int load_config_nemo(struct saa716x_dev *saa716x)
+{
+    int ret = 0;
+    return ret;
+}
+
+
 #define SAA716x_MODEL_TWINHAN_VP6090	"Twinhan/Azurewave VP-6090"
+#define SAA716x_CHIPS_TWINHAN_VP6090       "2xTDA8263 + 2xMB86A16L + 2xTDA8275A + 2xTDA10046 + SAA7162"
 #define SAA716x_DEV_TWINHAN_VP6090	"2xDVB-S + 2xDVB-T + 2xAnalog"
+
+/*****************NXP Semiconductors reference design board ******************/
+#define SAA716x_MODEL_NXP_NENO     "NXP Semiconductors NEMO referrence board" 
+#define SAA716x_CHIPS_NXP_NEMO      "SAA7160 + TDA8275A + TDA10046 + SAA7136"
+#define SAA716x_DEV_NXP_NEMO         "DVB-T + Analog"
+/*******************************END*************************************/
+
+
 
 static struct saa716x_config saa716x_vp6090_config = {
 	.model_name		= SAA716x_MODEL_TWINHAN_VP6090,
+	.chips_desc            = SAA716x_CHIPS_TWINHAN_VP6090,
 	.dev_type		= SAA716x_DEV_TWINHAN_VP6090,
 	.load_config		= &load_config_vp6090,
+};
+
+static struct saa716x_config saa716x_nemo_config = {
+       .model_name        = SAA716x_MODEL_NXP_NENO,
+	.chips_desc           = SAA716x_CHIPS_NXP_NEMO,
+	.dev_type             = SAA716x_DEV_NXP_NEMO,
+	.load_config          = &load_config_nemo,
 };
 
 
 static struct pci_device_id saa716x_hybrid_pci_table[] = {
 
 	MAKE_ENTRY(TWINHAN_TECHNOLOGIES, TWINHAN_VP_6090, SAA7160, &saa716x_vp6090_config),
+	MAKE_ENTRY(PCI_ANY_ID, PCI_ANY_ID, SAA7160, &saa716x_nemo_config),
 };
 MODULE_DEVICE_TABLE(pci, saa716x_hybrid_pci_table);
 
