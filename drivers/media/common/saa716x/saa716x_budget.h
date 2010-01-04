bcaddd5964717f64f9310586d86d3bd12935faa0
Manu Abraham <manu@linuxtv.org>
2010-01-04 16:26:49 +0400

    saa716x: Add support for the KNC1 Dual S2 device

--- drivers/media/common/saa716x/saa716x_budget.h
+++ drivers/media/common/saa716x/saa716x_budget.h
@@ -1,8 +1,11 @@
 #ifndef __SAA716x_BUDGET_H
 #define __SAA716x_BUDGET_H
- 
+
 #define TWINHAN_TECHNOLOGIES	0x1822
 #define TWINHAN_VP_1028		0x0044
 #define TWINHAN_VP_6002		0x0047
+
+#define KNC_One			0x1894
+#define KNC_Dual_S2		0x0110
 
 #endif /* __SAA716x_BUDGET_H */
