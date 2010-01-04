5ba4b559d7f9e1316fb2662a3e4368a607cd36cd
Manu Abraham <manu@linuxtv.org>
2010-01-04 22:49:19 +0400

    saa716x: Add video device support
    From: Andreas Regel <andreas.regel@gmx.de>

--- drivers/media/common/saa716x/saa716x_gpio.h
+++ drivers/media/common/saa716x/saa716x_gpio.h
@@ -9,6 +9,8 @@
 
 struct saa716x_dev;
 
+extern void saa716x_gpio_init(struct saa716x_dev *saa716x);
+
 extern u32 saa716x_gpio_rd(struct saa716x_dev *saa716x);
 extern void saa716x_gpio_wr(struct saa716x_dev *saa716x, u32 data);
 extern void saa716x_gpio_ctl(struct saa716x_dev *saa716x, u32 mask, u32 bits);
@@ -17,6 +19,7 @@ extern void saa716x_gpio_bits(struct saa716x_dev *saa716x, u32 bits);
 
 extern void saa716x_gpio_set_output(struct saa716x_dev *saa716x, int gpio);
 extern void saa716x_gpio_set_input(struct saa716x_dev *saa716x, int gpio);
+extern void saa716x_gpio_set_mode(struct saa716x_dev *saa716x, int gpio, int mode);
 extern void saa716x_gpio_write(struct saa716x_dev *saa716x, int gpio, int set);
 extern int saa716x_gpio_read(struct saa716x_dev *saa716x, int gpio);
 
