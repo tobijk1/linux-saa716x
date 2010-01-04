d1e79e52e07c8b7e10d5bea5fcf79038170c696c
Manu Abraham <manu@linuxtv.org>
2010-01-04 15:51:55 +0400

    saa716x: Initialize MSI

---
+++ drivers/media/common/saa716x/saa716x_msi.h
@@ -0,0 +1,7 @@
+#ifndef __SAA716x_MSI_H
+#define __SAA716x_MSI_H
+
+extern int saa716x_msi_init(struct saa716x_dev *saa716x);
+extern void saa716x_msiint_disable(struct saa716x_dev *saa716x);
+
+#endif /* __SAA716x_MSI_H */
