6ac49cc0d200942ce9b7c90822f43b4e873a1fc8
Andreas Regel <andreas.regel@gmx.de>
2011-11-25 19:39:03 +0100

    saa716x_ff: Reset the frontend on the SkyStar2 eXpress HD.
    From looking at photographs it seems that the RESETB pin of the
    STV0903 is connected to GPIO20 of the SAA7160ET.
    
    Signed-off-by: Andreas Regel <andreas.regel@gmx.de>

--- drivers/media/common/saa716x/saa716x_budget.c
+++ drivers/media/common/saa716x/saa716x_budget.c
@@ -490,6 +490,16 @@ static int skystar2_express_hd_frontend_attach(struct saa716x_adapter *adapter,
 		dprintk(SAA716x_DEBUG, 1, "Adapter (%d) Device ID=%02x", count,
 			saa716x->pdev->subsystem_device);
 
+		saa716x_gpio_set_output(saa716x, 20);
+
+		/* Reset the demodulator */
+		saa716x_gpio_write(saa716x, 20, 1);
+		msleep(10);
+		saa716x_gpio_write(saa716x, 20, 0);
+		msleep(10);
+		saa716x_gpio_write(saa716x, 20, 1);
+		msleep(10);
+
 		adapter->fe = dvb_attach(stv090x_attach,
 					 &skystar2_stv090x_config,
 					 &i2c->i2c_adapter,
