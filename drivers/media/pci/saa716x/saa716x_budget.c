933a05d2465310e107b53060a7043ef1869129b3
Andreas Regel <andreas.regel@gmx.de>
2013-02-24 13:36:59 +0100

    saa716x: section mismatch fixes
    fix attributes of init/exit functions to avoid section mismatches
    
    Signed-off-by: Andreas Regel <andreas.regel@gmx.de>
    
    [moved from media/common/saa716x/ to media/pci/saa716x/]
    Signed-off-by: Soeren Moch <smoch@web.de>

--- drivers/media/pci/saa716x/saa716x_budget.c
+++ drivers/media/pci/saa716x/saa716x_budget.c
@@ -643,15 +643,15 @@ static struct pci_driver saa716x_budget_pci_driver = {
 	.name			= DRIVER_NAME,
 	.id_table		= saa716x_budget_pci_table,
 	.probe			= saa716x_budget_pci_probe,
-	.remove			= saa716x_budget_pci_remove,
+	.remove			= __devexit_p(saa716x_budget_pci_remove),
 };
 
-static int __devinit saa716x_budget_init(void)
+static int __init saa716x_budget_init(void)
 {
 	return pci_register_driver(&saa716x_budget_pci_driver);
 }
 
-static void __devexit saa716x_budget_exit(void)
+static void __exit saa716x_budget_exit(void)
 {
 	return pci_unregister_driver(&saa716x_budget_pci_driver);
 }
