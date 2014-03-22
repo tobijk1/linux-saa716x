509b80710902393e2c563cbe6e37940ba2a959d6
Andreas Regel <andreas.regel@gmx.de>
2014-03-22 15:26:14 +0100

    saa716x_ff: implement PHI speedup
    There is a new module parameter 'phi_mode' to select different phi bus
    access modes to speedup data transfer to FPGA (OSD and TS stream in
    transfer/playback mode) and to reduce the CPU load accordingly.
    
    FPGA firmware 1.10 or newer required for non-default mode. On older
    versions only the PHI clock will be increased when a mode other than 0
    is selected.
    
    Based on patch by Soeren Moch <smoch@web.de>
    Signed-off-by: Andreas Regel <andreas.regel@gmx.de>

--- drivers/media/common/saa716x/saa716x_ff_phi.h
+++ drivers/media/common/saa716x/saa716x_ff_phi.h
@@ -3,6 +3,7 @@
 
 extern int saa716x_ff_phi_init(struct saa716x_dev *saa716x);
 extern void saa716x_ff_phi_exit(struct saa716x_dev *saa716x);
+extern void saa716x_ff_phi_config(struct saa716x_dev *saa716x);
 extern void saa716x_ff_phi_write(struct saa716x_dev *saa716x,
 				 u32 address, const u8 * data, int length);
 extern void saa716x_ff_phi_read(struct saa716x_dev *saa716x,
