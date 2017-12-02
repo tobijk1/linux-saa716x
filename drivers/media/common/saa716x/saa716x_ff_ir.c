b64bfc20a5944832eb55ae1b64a8f609ecc37936
Soeren Moch <smoch@web.de>
2017-12-02 21:23:34 +0100

    saa716x_ff: Convert to new timer API
    Convert to new timer API in linux-4.15.
    
    Signed-off-by: Soeren Moch <smoch@web.de>

--- drivers/media/common/saa716x/saa716x_ff_ir.c
+++ drivers/media/common/saa716x/saa716x_ff_ir.c
@@ -48,9 +48,9 @@ struct infrared {
 
 
 /* key-up timer */
-static void ir_emit_keyup(unsigned long parm)
+static void ir_emit_keyup(struct timer_list *t)
 {
-	struct infrared *ir = (struct infrared *) parm;
+	struct infrared *ir = from_timer(ir, t, keyup_timer);
 
 	if (!ir || !ir->key_pressed)
 		return;
@@ -184,9 +184,7 @@ int saa716x_ir_init(struct saa716x_dev *saa716x)
 	if (!ir)
 		return -ENOMEM;
 
-	init_timer(&ir->keyup_timer);
-	ir->keyup_timer.function = ir_emit_keyup;
-	ir->keyup_timer.data = (unsigned long) ir;
+	timer_setup(&ir->keyup_timer, ir_emit_keyup, 0);
 
 	input_dev = input_allocate_device();
 	if (!input_dev)
