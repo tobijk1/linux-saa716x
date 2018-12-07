64f875bcdf7c54536badd096d4defcc7e2617553
Soeren Moch <smoch@web.de>
2018-12-07 23:40:09 +0100

    saa716x_hybrid: remove incomplete Avermedia H-788 support
    Signed-off-by: Soeren Moch <smoch@web.de>

--- drivers/media/pci/saa716x/saa716x_hybrid.c
+++ drivers/media/pci/saa716x/saa716x_hybrid.c
@@ -551,33 +551,10 @@ static struct saa716x_config saa716x_averhc82_config = {
 	.i2c_rate		= SAA716x_I2C_RATE_100,
 };
 
-#define SAA716x_MODEL_AVERMEDIA_H788	"Avermedia H788"
-#define SAA716x_DEV_AVERMEDIA_H788	"DVB-T + Analaog"
-
-static int saa716x_averh88_frontend_attach(struct saa716x_adapter *adapter, int count)
-{
-	struct saa716x_dev *saa716x = adapter->saa716x;
-
-	dprintk(SAA716x_DEBUG, 1, "Adapter (%d) SAA716x frontend Init", count);
-	dprintk(SAA716x_DEBUG, 1, "Adapter (%d) Device ID=%02x", count, saa716x->pdev->subsystem_device);
-
-	return -ENODEV;
-}
-
-static struct saa716x_config saa716x_averh788_config = {
-	.model_name		= SAA716x_MODEL_AVERMEDIA_H788,
-	.dev_type		= SAA716x_DEV_AVERMEDIA_H788,
-	.adapters		= 1,
-	.frontend_attach	= saa716x_averh88_frontend_attach,
-	.irq_handler		= saa716x_hybrid_pci_irq,
-	.i2c_rate		= SAA716x_I2C_RATE_100,
-};
-
 static const struct pci_device_id saa716x_hybrid_pci_table[] = {
 
 	MAKE_ENTRY(TWINHAN_TECHNOLOGIES, TWINHAN_VP_6090, SAA7162, &saa716x_vp6090_config),
 	MAKE_ENTRY(AVERMEDIA, AVERMEDIA_HC82, SAA7160, &saa716x_averhc82_config),
-	MAKE_ENTRY(AVERMEDIA, AVERMEDIA_H788, SAA7160, &saa716x_averh788_config),
 	MAKE_ENTRY(KWORLD, KWORLD_DVB_T_PE310, SAA7162, &saa716x_atlantis_config),
 	MAKE_ENTRY(NXP_REFERENCE_BOARD, PCI_ANY_ID, SAA7162, &saa716x_atlantis_config),
 	MAKE_ENTRY(NXP_REFERENCE_BOARD, PCI_ANY_ID, SAA7160, &saa716x_nemo_config),
